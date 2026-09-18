38f3d42093ae38044cb7673113ada62d55d9256f
guiicorreiag <guiicorreiag@users.noreply.github.com>
2026-09-18 19:13:12 -0300

    Publish agenda interface

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/agenda-medica-liliane-site/sw.js', { scope: '/agenda-medica-liliane-site/' })})}
