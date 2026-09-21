2efb9c87bd982125d05d511dd7b00ee8c6861b71
guiicorreiag <guiicorreiag@users.noreply.github.com>
2026-09-21 10:54:44 -0300

    Publish Sao Paulo date fixes

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/agenda-medica-liliane-site/sw.js', { scope: '/agenda-medica-liliane-site/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
